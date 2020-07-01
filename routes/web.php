98a6d723f790cb183eb10c0c8bd8b8adaa0e9f4e
hanterrian <hanterrian@ukr.net>
2020-07-01 14:38:17 +0300

    Add sitemap & seo

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'WelcomeController@index')->name('main');
 
 Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('sitemap.xml', function () {
+    // create new sitemap object
+    $sitemap = App::make('sitemap');
+
+    // set cache key (string), duration in minutes (Carbon|Datetime|int), turn on/off (boolean)
+    // by default cache is disabled
+    $sitemap->setCache('laravel.sitemap', 60);
+
+    // check if there is cached sitemap and build new only if is not
+    if (!$sitemap->isCached()) {
+        // add item to the sitemap (url, date, priority, freq)
+        $sitemap->add(URL::to('/'), date('Y-m-d'), '1.0', 'daily');
+    }
+
+    // show your sitemap (options: 'xml' (default), 'html', 'txt', 'ror-rss', 'ror-rdf')
+    return $sitemap->render('xml');
+});
